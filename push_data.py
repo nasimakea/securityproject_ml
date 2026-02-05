397efa80e16d618271692b69fa4fa8b69224113d
nasimakea <nasimakram6200@gmail.com>
2026-02-05 23:24:16 +0530

    we have sent this to the mongodb

--- push_data.py
+++ push_data.py
@@ -1,22 +1,60 @@
-
-
-from dotenv import load_dotenv
-load_dotenv()
 import os
+import sys
+import json
+from dotenv import load_dotenv
 
+load_dotenv()
 
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-uri = os.getenv("MONGO_DB_URL")
+MONGO_DB_URL=os.getenv("MONGO_DB_URL")
+print(MONGO_DB_URL)
 
+import certifi
+ca=certifi.where()
 
+import pandas as pd
+import numpy as np
+import pymongo
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e :
+            raise NetworkSecurityException(e,sys)
+    def csv_to_json_convertor(self, file_path):
+        try :
+            data=pd.read_csv(file_path)
+            data.reset_index(drop=True,inplace=True)
+            records=list(json.loads(data.T.to_json()).values())
+            return records
+        except Exception as e :
+            raise NetworkSecurityException(e,sys)
+    def insert_data_mongodb(self,records,database,collection):
+        try:
+            self.database=database
+            self.collection=collection
+            self.records=records
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL) 
+            self.database=self.mongo_client[self.database]
+
+            self.collection=self.database[self.collection]
+            self.collection.insert_many(self.records)
+
+            return(len(self.records))
+        except Exception as e :
+            raise NetworkSecurityException(e,sys)
+        
+
+if __name__=='__main__':
+    FILE_PATH="network_data\phisingData.csv"
+    DATABASE="KRISHAI"
+    Collection="NetworkData"
+    networkobj=NetworkDataExtract()
+    records=networkobj.csv_to_json_convertor(file_path=FILE_PATH)
+    print(records)
+    no_of_records=networkobj.insert_data_mongodb(records,DATABASE, Collection)
+    print(no_of_records)
+    
